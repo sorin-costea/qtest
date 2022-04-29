48dd27d2c916c218bb6f956505e20cfc31c6bbae
Sorin I Costea <sorin-costea@users.noreply.github.com>
2022-04-30 01:50:08 +0200

    justatest

---
+++ src/test/java/test/qtest/AppTest.java
@@ -0,0 +1,17 @@
+package test.qtest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+@QuarkusTest
+public class AppTest {
+  @Test
+  public void testApp() {
+    assertTrue(true);
+  }
+}
